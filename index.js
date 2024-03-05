395485eb7fe5a253982761efc820b221e396a5e9
stiven <stivenhenaoronaldo1187@gmail.com>
2024-03-05 11:53:59 -0500

    he añadido un codigo para crear un array mediante la repeticion de un valor usando array.fill

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let array = {}
+let arrayVacio = {}
+
+let arrayConElementos =(
+    "1, 2, 3, 4, 5"
+);
+
+let arrayTiposDiferentes = ("1, dos, tres, false");
+
+let arrayConstructor = new array ("a, b, c");
+
+let secuenciaNumerica = array.from({ length: 5});
+(_, index) => index + 1;
